0ec9974d600351815add4618e53b7d62eed61286
Justin Garza <jgarza9788@gmail.com>
2025-12-21 12:05:43 -0500

    20251221

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cloudflare_link
 
+## install
+https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/do-more-with-tunnels/local-management/create-local-tunnel/
--- main.py
+++ main.py
@@ -1,57 +1,7 @@
 import os, sys, subprocess, time, re
 
 DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(DIR)
 
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-import os
-
-
-SCOPES = ["https://www.googleapis.com/auth/documents"]
-
-
-def get_creds():
-    creds = None
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    if not creds or not creds.valid:
-        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-        creds = flow.run_local_server(port=0)
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-    return creds
-
-def overwrite_doc(document_id, new_text):
-    creds = get_creds()
-    service = build("docs", "v1", credentials=creds)
-
-    # Get current doc length
-    doc = service.documents().get(documentId=document_id).execute()
-    end_index = doc.get("body")["content"][-1]["endIndex"]
-
-    requests = []
-    # Delete everything
-    if end_index > 1:
-        requests.append({
-            "deleteContentRange": {
-                "range": {"startIndex": 1, "endIndex": end_index - 1}
-            }
-        })
-    # Insert new text
-    requests.append({
-        "insertText": {
-            "location": {"index": 1},
-            "text": new_text
-        }
-    })
-
-    service.documents().batchUpdate(
-        documentId=document_id, body={"requests": requests}
-    ).execute()
-
-    print("Document replaced with new text!")
 
 def run_cmd(cmd):
     return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -77,8 +27,13 @@ def main():
     link = link.replace('"message":"|  ',"")
     print(link)
 
-    doc_id = "1cB9W6MXV5T4nBCEowRxvzE6Z3BIQouT2FALJiouczII"
-    overwrite_doc(doc_id, f"{link} - this link is good for the day")
+    ffpath = os.path.join(DIR,'overseerr_link.txt')
+
+    with open(ffpath,'w') as file:
+        file.write(link)
+
+    run_cmd(f'rclone copy {ffpath} drive_202508:temp/ ')
+
 
     # keep cloudflared alive
     proc.wait()
